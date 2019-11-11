21647b9e4c1e7e32831f07e10aac32b3b0200482
rizwanishaq <rizwanishaq@gmail.com>
2019-11-11 13:25:40 +0100

    first commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <spain className="subtitle">SHOP NOW</spain>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <spain className="subtitle">SHOP NOW</spain>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
+          <spain className="subtitle">SHOP NOW</spain>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMEN</h1>
+          <spain className="subtitle">SHOP NOW</spain>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MEN</h1>
+          <spain className="subtitle">SHOP NOW</spain>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
